562c360a248482d008275f5aa4fc6a02b5ea4442
misocactus <benjolisto@gmail.com>
2019-02-24 18:10:08 +1100

    second commit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
 
 type BlockChain struct {
@@ -23,13 +24,13 @@ func (b *Block) DeriveHash() {
 }
 
 func CreateBlock(data string, prevHash []byte) *Block { //* to say use this block
-	block := &Block{[]byte{}}, []byte(data), prevHash}
-	block.DeriveHash
+	block := &Block{ []byte{} , []byte(data), prevHash }
+	block.DeriveHash()
 	return block
 }
 
 func (chain *BlockChain) AddBlock(data string){
-	prevBlock := chain.block[len(chain.blocks)-1]
+	prevBlock := chain.blocks[len(chain.blocks)-1]
 	newBlock := CreateBlock(data, prevBlock.Hash)
 	chain.blocks = append(chain.blocks, newBlock)
 }
@@ -39,21 +40,21 @@ func Genesis() *Block {
 }
 
 func InitBlockChain() *BlockChain {
-	return &Blockchain{[]*Block{Genesis()}}
+	return &BlockChain{[]*Block{Genesis()}}
 }
 
 func main(){
-	chain := InitBlockChain
+	chain := InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.block {
+	for _, block := range chain.blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %S\n", block.Data)
+		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-
+		fmt.Println()
 	}
 
 }
